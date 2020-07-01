2d6d66ce3ae09ea64e2805b796f2bf7f43876701
grikkaq <melnikov@campus.technion.ac.il>
2020-07-01 18:21:28 +0300

    Add files via upload

--- elections_results.py
+++ elections_results.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 import itertools
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
-from sklearn.calibration import CalibratedClassifierCV
 # models that are being considered
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import StackingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
-from sklearn.ensemble import StackingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 
+"""
 def grid_search_cross_validation(x_train, y_train, grid, model):
     gridCV = GridSearchCV(model, grid, cv=10)
     gridCV.fit(x_train, y_train.T.squeeze())
@@ -31,7 +31,7 @@ def get_svc_best_params(x_train, y_train):
     }
     res = grid_search_cross_validation(x_train, y_train, grid, SVC())
     print(res)
-
+"""
 
 def print_accuracy_scores(performance_data):
     print('Accuracy scores:')
@@ -58,8 +58,8 @@ def cross_validation_acc_score(x, y, clf):
     score = cross_val_score(clf, x, y, cv=skfold)
     print('Accuracy {}%'.format(score.mean()*100))
 
-
-def predict_division_of_voters():
+"""
+def find_model():
     label='Vote'
     x_train = pd.read_csv("x_train.csv", header=0)
     y_train = pd.read_csv("y_train.csv", squeeze=True, header=None)
@@ -71,39 +71,83 @@ def predict_division_of_voters():
     x = x_train
     y = y_train
 
-
     # Best parameters for Random Tree Forest: {'criterion': 'gini', 'max_depth': 30, 'max_features': 'auto', 'min_samples_split': 2, 'n_estimators': 50}
     rand_forest_clf = RandomForestClassifier(criterion='gini', max_depth=50, min_samples_split=5, n_estimators=50)
+    cross_validation_acc_score(x, y, rand_forest_clf)
     rand_forest_clf.fit(x, y)
     prediction_rand_forest = rand_forest_clf.predict(x_valid)
-    cross_validation_acc_score(x, y, rand_forest_clf)
 
-
-    adabboost_svm_linear_clf = AdaBoostClassifier(base_estimator=SVC(kernel='linear', probability=True))
-    adabboost_svm_linear_clf.fit(x, y)
-    print('AdaBoost SVM Linear done fitting')
-    prediction_adaboost_linear = adabboost_svm_linear_clf.predict(x_valid)
-    print('AdaBoost SVM Linear done prediction')
-
-    # adabboost_lda_clf = AdaBoostClassifier(base_estimator=LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto'))
     # Best parameters for SVC {'degree': 4, 'kernel': 'poly'}
-    #adaboost_svm_poly_clf = AdaBoostClassifier(base_estimator=SVC(kernel='poly', degree=4, probability=True))
-    svm_poly_clf = base_estimator=SVC(kernel='poly', degree=4, probability=True)
+    svm_poly_clf = SVC(kernel='poly', degree=4, probability=True)
     svm_poly_clf.fit(x, y)
-    print('SVM Poly done fitting')
     prediction_svm_poly = svm_poly_clf.predict(x_valid)
-    print('SVM Poly done prediction')
-    #cross_validation_acc_score(x, y, svm_poly_clf)
-    #cross_validation_acc_score(x, y, adabboost_svm_linear_clf)
+
+    # Multi-layer perceptron classifier
+    perceptron_clf = MLPClassifier(activation="relu", alpha=0.1, hidden_layer_sizes=(10, 10, 10),
+                                   learning_rate="constant", max_iter=2000)
+    perceptron_clf.fit(x, y)
+    prediction_perceptron = perceptron_clf.predict(x_valid)
+
+
+    estimators = [
+        ('Random Forest',  RandomForestClassifier(criterion='gini', max_depth=50, min_samples_split=5, n_estimators=50)),
+        ('SVC', SVC(kernel='poly', degree=4, probability=True)),
+        ('Percepton', MLPClassifier(activation="relu", alpha=0.1, hidden_layer_sizes=(10, 10, 10),
+                                   learning_rate="constant", max_iter=2000))
+    ]
+    blend_clf = StackingClassifier(estimators)
+    blend_clf.fit(x, y)
+    prediction_blend = blend_clf.predict(x_valid)
     # evaluate and plot confusion matrices
+
     performance_data = [('Random Forest', prediction_rand_forest, y_valid),
-                        ('AdaBoost SVM Linear Kernel', prediction_adaboost_linear, y_valid),
-                        ('AdaBoost SVM Polinomial Kernel', prediction_svm_poly, y_valid)
+                        ('SVM Polinomial Kernel', prediction_svm_poly, y_valid),
+                        ('Perceptron', prediction_perceptron, y_valid),
+                        ('Blending ', prediction_blend, y_valid)
                         ]
     print_accuracy_scores(performance_data)
     print_f1_score(performance_data)
 
-    prediction = prediction_rand_forest
+    prediction = prediction_blend
+    parties = np.unique(prediction)
+
+    num_votes_for_party = lambda party: len([vote for vote in prediction if vote == party])
+    list_of_parties = [(party, num_votes_for_party(party)) for party in parties]
+    num_votes = len(y_test.index)
+
+    winner = max(list_of_parties, key=lambda item: item[1])
+    print('Party with most probable majority of votes')
+    print(winner[0], ':', winner[1], ',', winner[1] * 100 / num_votes, '%')
+
+    # 2. Division of voters between the parties
+    print('Amount of votes per party')
+    for party_votes in sorted(list_of_parties, key=lambda votes: votes[1], reverse=True):
+        print(party_votes[0], ':', party_votes[1], ',', party_votes[1] * 100 / num_votes, '%')
+"""
+
+if __name__ == '__main__':
+    label = 'Vote'
+    x_train = pd.read_csv("x_train.csv", header=0)
+    y_train = pd.read_csv("y_train.csv", squeeze=True, header=None)
+    x_valid = pd.read_csv("x_valid.csv", header=0)
+    y_valid = pd.read_csv("y_valid.csv", squeeze=True, header=None)
+    x_test = pd.read_csv("x_test.csv", header=0)
+    y_test = pd.read_csv("y_test.csv", squeeze=True, header=None)
+    # get_random_forest_best_params(x_train, y_train)
+    x = x_train
+    y = y_train
+
+    estimators = [
+        ('Random Forest',  RandomForestClassifier(criterion='gini', max_depth=50, min_samples_split=5, n_estimators=50)),
+        ('SVC', SVC(kernel='poly', degree=4, probability=True)),
+        ('Percepton', MLPClassifier(activation="relu", alpha=0.1, hidden_layer_sizes=(10, 10, 10),
+                                   learning_rate="constant", max_iter=2000))
+    ]
+    blend_clf = StackingClassifier(estimators)
+    blend_clf.fit(x, y)
+    prediction = blend_clf.predict(x_test)
+    # evaluate and plot confusion matrices
+
     parties = np.unique(prediction)
 
     num_votes_for_party = lambda party: len([vote for vote in prediction if vote == party])
@@ -119,6 +163,7 @@ def predict_division_of_voters():
     for party_votes in sorted(list_of_parties, key=lambda votes: votes[1], reverse=True):
         print(party_votes[0], ':', party_votes[1], ',', party_votes[1] * 100 / num_votes, '%')
 
+    performance_data = [('Blending ', prediction, y_test)]
+    print_accuracy_scores(performance_data)
+    print_f1_score(performance_data)
 
-if __name__ == '__main__':
-    predict_division_of_voters()
